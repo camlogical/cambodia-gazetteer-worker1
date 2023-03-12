6703a6935c6eaaf50b5ee7637a3fcc8580a0eaa5
Seanghay Yath <seanghay.dev@gmail.com>
2023-03-12 10:42:51 +0700

    feat: add vite

---
+++ src/app.js
@@ -0,0 +1,13 @@
+import '../styles/reset.css'
+import '../styles/app.css'
+
+async function main() {
+  console.time('download')
+  const url = "https://cdn.jsdelivr.net/gh/seanghay/cambodia-gazetteer-worker@main/data/all.minified.json";
+  const res = await fetch(url);
+  const data = await res.json();
+  console.timeEnd('download');
+  console.log(data);
+}
+
+main();
